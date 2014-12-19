7df1a30c7beff7b64936f65a7ec83caa64fb2c4e
Bruno Luiz da Silva <contato@brunoluiz.net>
2014-12-19 18:00:37 -0200

    Working but sometimes it is giving error on transfering files

--- transfer.sh
+++ transfer.sh
@@ -11,13 +11,14 @@ else
 fi
 
 echo "[SD] Transfering Debian img to device"
-sudo dd if=bone-debian-7.5-2014-05-14-2gb.img of="$1" bs=4M
+sudo dd if=bone-debian-7.5-2014-05-14-2gb.img of="$1" bs=1M
+sync
 
 echo "[SD] Transfering Linux PREEMPT RT Kernel to boot/"
 mkdir boot
 sudo mount "$1"1 boot
 cp -f linux-3.12.31-rt45/arch/arm/boot/zImage boot/           # Replacing zImage
-rm -rf boot/dtbs/*                                            # Removing the old dtbs
+sudo rm -rf boot/dtbs/*                                       # Removing the old dtbs
 cp -rf linux-3.12.31-rt45/arch/arm/boot/dts/*.dtb boot/dtbs   # Copying new dtbs
 cp -rf linux-3.12.31-rt45/arch/arm/boot/dts/*.dts boot/dtbs   # Copying new dtbs
 sudo umount boot
@@ -26,6 +27,7 @@ rm -rf boot
 echo "[SD] Transfering Linux Modules to rootfs/"
 mkdir rootfs
 sudo mount "$1"2 rootfs
+sudo rm -rf rootfs/lib/modules
 cp -rf linux_modules/* rootfs/
 sudo umount rootfs
 rm -rf rootfs
